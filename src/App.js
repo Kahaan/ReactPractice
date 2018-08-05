bbdbda81b2842d10964a39ce00c08d26612c8a85
Kahaan <kahaan.r.patel@gmail.com>
2018-08-05 12:49:15 -0700

    react-redux

--- src/App.js
+++ src/App.js
@@ -2,23 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Posts from './components/posts'
+import PostForm from './components/post_form'
 
 class App extends Component {
-
-  constructor(props){
-    super(props)
-    this.state = {
-      posts: []
-    }
-
-  }
-
-
-  componentWillMount(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
-      .then(data => this.setState({posts: data}))
-  }
 
   render() {
     return (
@@ -27,6 +13,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <PostForm />
+        <hr />
         <Posts />
       </div>
     );
