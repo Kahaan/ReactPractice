843bf054f80a1506d83ad45f82744b8f7ff23953
Kahaan <kahaan.r.patel@gmail.com>
2018-08-05 11:58:45 -0700

    react-redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Posts from './components/posts'
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      posts: []
+    }
+
+  }
+
+
+  componentWillMount(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => this.setState({posts: data}))
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +27,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Posts />
       </div>
     );
   }
